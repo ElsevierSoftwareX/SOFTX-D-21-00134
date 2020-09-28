f221f4a2924596a7d648838f286a205a36ec8a84
Simon Morgenthaler <s.morgenthaler@fz-juelich.de>
2020-09-28 15:09:13 +0200

    export calliope first draft

--- emipy/export.py
+++ emipy/export.py
@@ -7,6 +7,7 @@ Created on Thu Sep 17 09:17:58 2020
 
 import yaml
 import pandas as pd
+import copy
 
 def get_default_config():
     """
@@ -43,18 +44,54 @@ def get_default_config():
                     'interest_rate': 0,
                     'om_prod': 0.07
                     }}}}}
+
+    # For now, let's focus on supply only!
+    d['techs'].pop('amine_scrubbing')
     return d
 
 
-def prepare_csv(data, year):
+def prepare_csv(data):
     """
     """
-    if not year in data.ReportingYear.unique():
-        return print('Data does not match.')
-    df = pd.DataFrame(index=pd.date_range(start=str(year),
-                                          end=str(year)+'-12-31 23',
+    years = sorted(data.ReportingYear.unique())
+    df = pd.DataFrame()
+    for y in years:
+        df0 = pd.DataFrame(index=pd.date_range(start=str(y),
+                                          end=str(y)+'-12-31 23',
                                           freq='H'))
-    for i, row in data.iterrows():
-        column_name = row.FacilityName.replace(' ', '_').replace('.', '_').replace(',', '_').replace('-', '_')
-        df[column_name] = row.TotalQuantity/len(df)
-    return df
+        df = pd.concat([df, df0])
+
+    coords = {}
+    for y in years:
+        for i, row in data[data.ReportingYear == y].iterrows():
+            column_name = row.FacilityName.replace(' ', '_').replace('.', '_').replace(',', '_').replace('-', '_').replace('\&', '_').replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue')
+            df.loc[str(y), column_name] = row.TotalQuantity/len(df)
+            coords[column_name] = (row.Lat, row.Long)
+    return df, coords
+
+
+def export_calliope(data, sc=0.07):
+    """
+    """
+
+    df, coords = prepare_csv(data)
+    d = get_default_config()
+    config = {}
+    
+    for c in df.columns:
+        config[c] = copy.deepcopy(d)
+        s = 'emipy.csv:' + str(c)
+        config[c]['techs']['co2_supply']['constraints']['resource'] = s
+        config[c]['techs']['co2_supply']['constraints']['energy_cap_max'] = float(df.loc[:, c].max())
+        config[c]['coordinates'] = {'lat': coords.get(c)[0], 'lon': coords.get(c)[1]}
+        config[c]['techs']['co2_supply']['costs']['monetary']['om_prod'] = sc
+
+    with open('emipy2calliope.yaml', 'w') as f:
+        yaml.dump(config, f, default_flow_style=False)
+
+    df.to_csv('emipy.csv')
+
+    return None
+
+
+    
