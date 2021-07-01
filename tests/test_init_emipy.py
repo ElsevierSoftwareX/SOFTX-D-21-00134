d897c2794fe61e7c41f1ca7a144a67b46b560207
f.overberg <f.overberg@fz-juelich.de>
2021-07-01 23:52:11 +0200

    update test_init_emipy.py

--- tests/test_init_emipy.py
+++ tests/test_init_emipy.py
@@ -36,11 +36,11 @@ class TestCompleteInitiation:
 
     def test_init_emipy_4(self):
         # Test if the pollutin data is downloaded in the initiation process.
-        assert len(os.listdir(os.path.join(RootPath, 'PollutionData'))) == 17
+        assert len(os.listdir(os.path.join(RootPath, 'PollutionData'))) == 18
 
     def test_init_emipy_5(self):
         # Test if the mapping data is downloaded in the initiation process.
-        assert len(os.listdir(os.path.join(RootPath, 'MappingData'))) == 1365
+        assert len(os.listdir(os.path.join(RootPath, 'MappingData'))) == 1214
 
     def test_init_emipy_6(self):
         # Test if the transition data is downloaded in the initiation process.
@@ -49,17 +49,17 @@ class TestCompleteInitiation:
     def test_init_emipy_7(self):
         # Test if pratr.pkl, fr.pkl and pr.pkl is generated in the initiation process.
         i = 0
-        if os.path.isfile(os.path.join(RootPath, 'PollutionData\\pratr.pkl')):
+        if os.path.isfile(os.path.join(RootPath, 'PollutionData', 'pratr.pkl')):
             i = i + 1
-        if os.path.isfile(os.path.join(RootPath, 'PollutionData\\fr.pkl')):
+        if os.path.isfile(os.path.join(RootPath, 'PollutionData', 'fr.pkl')):
             i = i + 1
-        if os.path.isfile(os.path.join(RootPath, 'PollutionData\\pr.pkl')):
+        if os.path.isfile(os.path.join(RootPath, 'PollutionData', 'pr.pkl')):
             i = i + 1
         assert i == 3
 
     def test_init_emipy_8(self):
         # Test if db.pkl is generated in the initiation process.
-        assert os.path.isfile(os.path.join(RootPath, 'PollutionData\\db.pkl')) is True
+        assert os.path.isfile(os.path.join(RootPath, 'PollutionData', 'db.pkl')) is True
 
     def test_init_emipy_9(self):
         # Test if directory 'ExportData' is generated in the initiation process.
@@ -70,7 +70,7 @@ class TestCompleteInitiation:
         foo = os.getcwd()
         ep.download_MapData(RootPath, resolution=60)
         os.chdir(foo)
-        assert len(os.listdir(os.path.join(RootPath, 'MappingData'))) == 2120
+        assert len(os.listdir(os.path.join(RootPath, 'MappingData'))) == 1969
 
 class TestReadData:
 
@@ -86,7 +86,7 @@ class TestReadData:
 
     def test_read_db2(self):
         # Test if the complete data set is loadable (with specifing the path) and has 470203 rows and 73 columns.
-        db = ep.read_db(RootPath)
+        db = ep.read_db(os.path.join(RootPath, 'PollutionData', 'db.pkl'))
         i = 0
         if len(db) == 470203:
             i = i + 1
@@ -101,7 +101,7 @@ class TestReadData:
 
     def test_read_mb1(self):
         # Test if basic mapbase is loadable (with specifing the path) and has 37 rows and 10 columns.
-        mb = ep.read_mb(RootPath)
+        mb = ep.read_mb(os.path.join(RootPath, 'MappingData', 'NUTS_BN_10M_2006_3035.shp'))
         i = 0
         if len(mb) == 37:
             i = i + 1
